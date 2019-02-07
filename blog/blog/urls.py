d66783fb16736de4e92895cde4ec566fbee2f61f
Iain Goh <iaingoh@gmail.com>
2019-02-07 19:20:13 +0800

    Removed faulty link, user_registration and added a post through admin

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -12,5 +12,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('templates_corey_schafer.urls')),
-    path('user_registration/', include('user_registration.urls'))
 ]
